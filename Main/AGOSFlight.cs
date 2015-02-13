fe8c9a92950bf6462ca305c8719ea7f6631a7b08
iPeer <chrisdwignall@live.com>
2015-02-13 17:42:48 +0000

    Basic Rewrite Framework

---
+++ Main/AGOSFlight.cs
@@ -0,0 +1,33 @@
+﻿using iPeerLib.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace AGroupOnStage.Main
+{
+    [KSPAddon(KSPAddon.Startup.Flight, false)]
+    class AGOSFlight : MonoBehaviour
+    {
+
+        public void Start()
+        {
+            Logger.Log("Start!");
+            GameEvents.onStageActivate.Add(OnStageActivate);
+            GameEvents.onStageSeparation.Add(OnStageSeparation);
+        }
+
+        private void OnStageActivate(int stage)
+        {
+            Logger.Log("OnStageActivate: " + stage);
+        }
+
+        private void OnStageSeparation(EventReport e)
+        {
+
+            Logger.Log("OnStageSeparation: "+e.stage);
+        }
+
+    }
+}
---
+++ Main/AGOSMain.cs
@@ -0,0 +1,42 @@
+﻿using AGroupOnStage.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace AGroupOnStage.Main
+{
+    [KSPAddon(KSPAddon.Startup.SpaceCentre, true)]
+    class AGOSMain : MonoBehaviour
+    {
+
+
+        static AGOSMain Instance { get; private set; }
+
+        public void Start()
+        {
+            Logger.Log("AGOS Main Start()");
+            Instance = this;
+        }
+
+        private void setupToolbarButton()
+        {
+
+        }
+
+        private void removeToolbarButton()
+        {
+
+        }
+
+        private void toggleGUI()
+        {
+
+        }
+
+        private void OnGUI() { }
+        private void OnWindow(int windowID) { }
+
+    }
+}
