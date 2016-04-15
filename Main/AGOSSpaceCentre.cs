db201fb8e1833b0c48294db4a596a1e484424af9
iPeer <chrisdwignall@live.com>
2016-04-15 04:43:00 +0100

    GUI Rendering support for SpaceCentre scene

---
+++ Main/AGOSSpaceCentre.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using AGroupOnStage.Logging;
+
+namespace AGroupOnStage.Main
+{
+    [KSPAddon(KSPAddon.Startup.SpaceCentre, false)]
+    class AGOSSpaceCentre : MonoBehaviour
+    {
+
+        public static AGOSSpaceCentre Instance { get; protected set; }
+
+        public void Start()
+        {
+            Logger.Log("AGOS.Main.AGOSSpaceCentre.Start()");
+            if (Instance == null)
+                Instance = this;
+        }
+
+        private void OnGUI()
+        {
+            AGOSUtils.renderVisibleGUIs();
+        }
+
+    }
+}
